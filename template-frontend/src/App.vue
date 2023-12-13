2f4ee30475d1f754a797b920d535d14b5d82caf4
jocularchao <jocularchao@gmail.com>
2023-12-13 22:46:02 +0800

    简单布局

--- template-frontend/src/App.vue
+++ template-frontend/src/App.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
+  <!--总体框架-->
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <!--左半边-->
+    <div style="flex: 1;background-color: antiquewhite">
 
+    </div>
+    <!--右半边 固定宽度-->
+    <div style="width: 300px;background-color: aquamarine">
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
